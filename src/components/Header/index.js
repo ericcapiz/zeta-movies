4a51a8d190213b0cfa2699531fd5b124ddb88433
ericcapiz <ericcapiz@gmail.com>
2021-07-09 07:00:06 -0500

    routing setup

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
-
-import TMDBLogo from '../../images/tmdb_logo.svg'
-
-import {Wrapper, Content, Logo, TMDBLogoImg} from './Header.styles'
+import {Link} from 'react-router-dom';
+import TMDBLogo from '../../images/tmdb_logo.svg';
+import {Wrapper, Content, Logo, TMDBLogoImg} from './Header.styles';
 
 
 const Header = () => {
     return (
         <Wrapper>
             <Content>
-                <TMDBLogoImg src={TMDBLogo} alt="rmdb-logo" />
-                <Logo>Zeta Films </Logo>
+                <a href="https://www.themoviedb.org/" target="_blank">
+                    <TMDBLogoImg src={TMDBLogo} alt="rmdb-logo" />
+                </a>
+                <Link style={{textDecoration: "none"}} to="/">
+                    <Logo>Zeta Films </Logo>
+                </Link>
             </Content>
         </Wrapper>
     )
