d5acec18e6b8dc9d4d4adbdc1059db8497388a76
ericcapiz <ericcapiz@gmail.com>
2021-07-09 05:32:04 -0500

    hero img complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,7 @@
 import React from 'react';
-
 import {POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from '../config';
-
 import {useHomeFetch} from '../hooks/useHomeFetch';
-
+import HeroImage from './HeroImage';
 import NoImage from '../images/no_image.jpg';
 
 
@@ -11,9 +9,15 @@ const Home = () => {
    const {state, loading, error} = useHomeFetch()
 
     return (
-        <div>
-            home
-        </div>
+        <>
+            {state.results[1] ? 
+                <HeroImage
+                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[1].backdrop_path}`}
+                    title={state.results[1].original_title}
+                    text={state.results[1].overview}
+                 /> : null
+            }
+        </>
     )
 }
 
--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ const SESSION_ID_URL = `${API_URL}authentication/session/new?api_key=${API_KEY}`
 
 const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
 // Sizes: w300, w780, w1280, original
-const BACKDROP_SIZE = 'w1280';
+const BACKDROP_SIZE = 'w780';
 // w92, w154, w185, w342, w500, w780, original
 const POSTER_SIZE = 'w780';
 
